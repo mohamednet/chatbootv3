6a22c1753ce8554ebe355846bdbc2beeafd20891
_Aress_ <mohamed.anattahe@gmail.com>
2025-02-11 19:12:30 +0100

    Add logging to scheduler for reminder jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -17,9 +18,19 @@ class Kernel extends ConsoleKernel
         // Run every second to check for messages to process
         $schedule->command('messages:process')->everySecond();
 
+        // Run trial reminders every hour
+        $schedule->call(function () {
+            Log::info('Scheduling TrialReminderJob');
+            dispatch(new \App\Jobs\TrialReminderJob());
+        })->hourly();
+
+        // Run marketing reminders every hour
+        $schedule->call(function () {
+            Log::info('Scheduling MarketingReminderJob');
+            dispatch(new \App\Jobs\MarketingReminderJob());
+        })->hourly();
+
         // Reminder Jobs
-        $schedule->job(new TrialReminderJob)->hourly();
-        $schedule->job(new MarketingReminderJob)->hourly();
         $schedule->job(new PaidSubscriptionReminderJob)->hourly();
     }
 
