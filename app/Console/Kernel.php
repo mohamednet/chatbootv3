44f5f5cfe898af52e666d59f0ee788f8a65a59a8
_Aress_ <mohamed.anattahe@gmail.com>
2025-04-01 16:36:19 +0000

    erazraz

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new \App\Jobs\ProcessTrialRemindersJob)
+        $schedule->command('trials:process-reminders')
                 ->everyFiveMinutes();
     }
 
